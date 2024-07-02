86a6ccb61301aeca357c58b0edb68959ad491222
shivendra1751@gmail.com <shivendra1751@gmail.com>
2024-07-02 17:08:47 +0530

    [ADD]:To Do

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import { useState } from "react"; 
 import AddTask from "./components/AddTask"
+import ToDo from "./components/ToDo"
+
 
 function App() {
+  const[taskList, setTaskList] = useState([])
+  console.log(taskList)
+
   return (
     <>
       <h1 className="text-2xl font-bold py-4 pl-6">03 - The Task Tracker</h1>
       <p className="text-xl pl-6">Hi there!</p>
       <div className="flex flex-row items-center">
         <p className="text-xl pl-6">Click</p>
-        <AddTask/> 
-        <p>to add a new task</p>
+        <AddTask taskList={taskList} setTaskList={setTaskList}/> 
+        <p className="text-xl my-2">to add a new task</p>
+      </div>
+      <div>
+        <h2 className="ml-6 text-xl font-semibold w-3/4
+        max-w-lg my-4 py-2 px-4 bg-gray-200">To Do:</h2>
+      {
+        taskList.map((task,i)=>
+          <>
+             <ToDo key={i} task={task}/>
+          </>
+        )
+      }
       </div>
     </>
   );
